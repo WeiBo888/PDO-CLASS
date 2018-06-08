002276ef1a968ffaa6407a8ba51a8f3fb3b11cee
WeiBo888 <704958255@qq.com>
2018-06-08 12:14:18 +0800

    创建代码测试文件

--- database.php
+++ database.php
@@ -169,18 +169,4 @@ class DB{
 
 }
 
-$db = new DB('127.0.0.1','mysql_basic','root','****');
 
-//echo $db->add('jr_rubbish',['fistname' => 'jj', 'lastname' => 'linjunjie', 'email' => 'jj@163.com']);
-
-//echo $db->delete('jr_rubbish',['lastname' => ['like', '%hui%'], 'id' => ['>', 102]]);
-
-//$db->update('jr_rubbish',['fistname' => 'TATA','lastname'=>'HH'],['id' => ['=',139]]);
-
-$db->search('','jr_rubbish',['id' => ['=', 139]]);
-
-//$db->checkWhere([
-//    'name' => ['=','Tom'],
-//    'age' => ['=', 18],
-//    'hobby' => ['like','%_篮球']
-//]);
